5be87fe722af0cc5d0be47387ffd8f7886f54537
link375 <https://github.com/link375>
2018-04-08 08:56:25 -0600

    check for most starred project

--- python_repos.py
+++ python_repos.py
@@ -13,20 +13,33 @@ print("Total repositories:", response_dict['total_count'])
 
 # Explore information about the repositories
 repo_dicts = response_dict['items']
+
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+#Make visualization
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legent=False)
+chart.title = "Most - Starred Python Projects on Github"
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
+
+# Print how many repos are available
 print("Repositories returned:", len(repo_dicts))
 
 # Examine the first repository
 repo_dict = repo_dicts[0]
 
 print("\nSelected information about first repository:")
-for repo_dict in repo_dicts:
-    print('Name', repo_dict['name'])
-    print('Owner', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Created:', repo_dict['created_at'])
-    print('Updated:', repo_dict['updated_at'])
-    print('Description:', repo_dict['description'])
+print('Owner', repo_dict['owner']['login'])
+print('Repository:', repo_dict['html_url'])
+print('Created:', repo_dict['created_at'])
+print('Updated:', repo_dict['updated_at'])
+print('Description:', repo_dict['description'])
 
 
 print("\nKeys:", len(repo_dict))
